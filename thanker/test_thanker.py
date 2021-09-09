dd58d50de87cb927f689456bcfd531dcac0d7f54
WardPearce <wardpearce@protonmail.com>
2021-09-10 11:24:25 +1200

    Added unit tests

--- thanker/test_thanker.py
+++ thanker/test_thanker.py
@@ -7,8 +7,20 @@ class TestThanker(asynctest.TestCase):
     use_default_loop = True
 
     async def setUp(self) -> None:
-        self.thanker = Thanker(["aiohttp"])
+        self.thanker = Thanker(["thanker"])
 
     async def test_style(self) -> None:
         async with self.thanker as thanks:
             self.assertIsInstance(await thanks.style(), str)
+
+    async def test_package_info(self) -> None:
+        async with self.thanker as thinks:
+            self.assertIsInstance(
+                await thinks.package_info("aiohttp"),
+                dict
+            )
+
+    async def test_scan(self) -> None:
+        async with self.thanker as thinks:
+            async for info in thinks.scan("aiohttp"):
+                self.assertIsInstance(info, dict)
